ef8bd943d97105bf50a77511b16107b8e183038a
Matthew-kachensky <awesomemaffoo@gmail.com>
2023-03-04 12:55:57 -0700

    Pulling

---
+++ components/Plant.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import { View } from 'react-native';
+
+const plantTypes = { tomato :  };
+
+
+const styles = StyleSheet.create({
+    container : {
+        paddingTop: 25,
+    },
+    image : {
+        width: 50,
+        height: 50,
+    },
+});
+class Plant extends Component {
+
+
+
+    render() {
+        <View style = {styles.image}>
+            <Image style = {styles.image}
+            source={require('../pictures/' + )}/>
+        </View>
+    }
+};
